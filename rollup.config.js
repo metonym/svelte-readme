528c05992a0bc194ffe23518cf70dc8806165959
Eric Liu <ericyl.us@gmail.com>
2020-11-23 12:03:25 -0800

    feat: autogenerate public/index.html and hash bundle in prod
    Closes #3

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,14 @@ import pkg from "./package.json";
 
 export default {
   input: "src/index.js",
-  external: [...Object.keys(pkg.dependencies), "fs", "path", "svelte/compiler", "prismjs/components/prism-bash"],
+  external: [
+    ...Object.keys(pkg.dependencies),
+    "crypto",
+    "fs",
+    "path",
+    "svelte/compiler",
+    "prismjs/components/prism-bash",
+  ],
   output: {
     format: "cjs",
     file: pkg.main,
--- src/createConfig.js
+++ src/createConfig.js
@@ -3,8 +3,20 @@ import rollupPluginSvelte from "rollup-plugin-svelte";
 import virtual from "@rollup/plugin-virtual";
 import { terser } from "rollup-plugin-terser";
 import { preprocessReadme } from "./preprocessReadme";
-import fs from "fs";
+import fs from "fs-extra";
 import path from "path";
+import { createHash } from "crypto";
+
+function hashREADME() {
+  try {
+    const path_readme = path.join(process.cwd(), "README.md");
+    const readme = fs.readFileSync(path_readme);
+    return "." + createHash("md5").update(readme).digest("hex").slice(0, 8);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
 
 function getPackageJSON() {
   try {
@@ -16,6 +28,7 @@ function getPackageJSON() {
     return {
       name: pkg.name,
       svelte: pkg.svelte,
+      description: pkg.description,
     };
   } catch (error) {
     console.log(error);
@@ -24,13 +37,41 @@ function getPackageJSON() {
 }
 
 export default function createConfig(opts) {
+  const minify = opts.minify === true;
+  const pkg = getPackageJSON();
+  const hash = minify ? hashREADME() : "";
+
+  fs.ensureFileSync("public/index.html");
+  fs.writeFileSync(
+    "public/index.html",
+    `
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content="${pkg.description || `${pkg.name} demo`}" />
+      <title>${pkg.name}</title>
+      <link
+        href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/4.0.0/github-markdown.min.css"
+        rel="stylesheet"
+      />
+    </head>
+    <body>
+      <noscript>You need to enable JavaScript to run this app.</noscript>
+      <script src="bundle${hash}.js"></script>
+    </body>
+  </html>
+`.trim()
+  );
+
   return {
     watch: { clearScreen: false },
     input: "entry",
     output: {
       format: "iife",
       name: "app",
-      file: "public/bundle.js",
+      file: `public/bundle${hash}.js`,
       ...(opts.output || {}),
     },
     plugins: [
@@ -42,11 +83,11 @@ export default function createConfig(opts) {
       rollupPluginSvelte({
         ...(opts.svelte || {}),
         extensions: [".svelte", ".md"],
-        preprocess: [preprocessReadme({ ...getPackageJSON(), prefixUrl: opts.prefixUrl })],
+        preprocess: [preprocessReadme({ ...pkg, prefixUrl: opts.prefixUrl })],
       }),
       resolve(),
       ...(opts.plugins || []),
-      opts.minify === true && terser(),
+      minify && terser(),
     ].filter(Boolean),
   };
 }
